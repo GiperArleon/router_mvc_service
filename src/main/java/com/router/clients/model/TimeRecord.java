58e31bc09d9d1af41111c9336fddad74f6fb65c2
Artem <42321627+GiperArleon@users.noreply.github.com>
2022-10-16 20:23:49 +0300

    bug fixes

--- src/main/java/com/router/clients/model/TimeRecord.java
+++ src/main/java/com/router/clients/model/TimeRecord.java
@@ -3,7 +3,6 @@ package com.router.clients.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -11,7 +10,7 @@ import java.time.LocalDate;
 public class TimeRecord {
     private Integer id;
     private Integer userId;
-    private LocalDate date;
+    private String date;
     private String description;
     private Integer hours;
     private Integer minutes;
